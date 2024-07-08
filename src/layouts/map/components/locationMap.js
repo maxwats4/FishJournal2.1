7c72488c7212e9506991c106b7918a155df23c53
Max Watson <maxwats4@gmail.com>
2024-07-08 08:22:35 -0600

    Map working, Old Code refactored and removed to maintain codebase

--- src/layouts/map/components/locationMap.js
+++ src/layouts/map/components/locationMap.js
@@ -157,69 +157,9 @@ function arrayToMarkers() {
     updatedMarkers.push(tempLocation);
   });
 
-  return TempUpdatedMarkers; // Return the updated markers array
-}
-// put these methods on the use effect
-
-
-/**
- * Old functions for the map
- *
- */
-
-//Pulls live weather data and puts it into locations
-// change parameters to lat and long
-async function fetchWeatherData(index) {
-  const apiUrl =
-    "https://api.openweathermap.org/data/2.5/weather?lat=" +
-    window.Locations[index].getLatitude() +
-    "&lon=" +
-    window.Locations[index].getLongitude() +
-    "&units=imperial&appid=8003f2b648dfae7cf096951064b5a093";
-
-  try {
-    const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-
-    const data = await response.json();
-    // Additional code for updating global objects based on index
-    //Wind speed need to be in miles per hour, not meters per second
-
-    updateGlobalObject(
-      index,
-      data.clouds.all,
-      data.main.temp,
-      data.wind.speed,
-      data.weather[0].description
-    );
-  } catch (error) {
-    console.error("Error:", error);
-  }
+  
 }
 
-// Function to update the values of an object in the array based on index
-const updateGlobalObject = (
-  index,
-  newCloudRating,
-  newLocationTemp,
-  newLocationWind,
-  newLocationWeatherConditions
-) => {
-  // Make a copy of the current state
-  const updatedObjects = [...window.Locations];
-
-  // Update the values of the object at the specified index
-  updatedObjects[index].locationCloudRating = newCloudRating;
-  updatedObjects[index].setLocationTemp(newLocationTemp);
-  updatedObjects[index].setLocationWind(newLocationWind);
-  updatedObjects[index].setLocationWeatherConditions(
-    newLocationWeatherConditions
-  );
-  // Update the state, triggering a re-render
-  window.Locations = updatedObjects;
-};
 
 // basically the main method
 // this is JXL
@@ -228,16 +168,8 @@ export default function LocationMap() {
 
   // loops through the global array and updates all of the objects with live data
   useEffect(() => {
+  
     const fetchDataAndUpdate = async () => {
-      for (let i = 0; i < window.Locations.length; i++) {
-        fetchWeatherData(i);
-      }
-      setTimeout(() => {
-        
-      }, 1000);
-    };
-
-    const fetchDataAndUpdate2 = async () => {
       getLocationObjects(database, userId, locationList)
       .then((locations) => {
         // Create an array of promises for updating the weather
@@ -264,92 +196,11 @@ export default function LocationMap() {
       });
       };
 
-      // setLoading(false);
-    fetchDataAndUpdate2();
+     
     fetchDataAndUpdate();
   }, []); // The empty dependency array ensures that this effect runs only once, similar to componentDidMount
 
-  //Popup markers
-  const markers = [
-    {
-      geocode: [44.428767, -111.356669],
-      popUp: (
-        <p>
-          Location: {window.LocationBuffaloDock.getName()}
-          <br />
-          Current Weather Conditions:{" "}
-          {window.LocationBuffaloDock.getLocationWeatherConditions()}
-          <br />
-          Cloud Rating: {window.LocationBuffaloDock.getLocationCloudRating()}%
-          <br />
-          Current Temp: {window.LocationBuffaloDock.getLocationTemp()} degrees
-          <br />
-          Current Wind: {window.LocationBuffaloDock.getLocationWind()} MpH
-        </p>
-      ),
-      Icon: customIconRed,
-    },
-    {
-      geocode: [44.500751, -111.33618],
-      popUp: (
-        <p>
-          Location: {window.LocationMacksInn.getName()}
-          <br />
-          Current Weather Conditions:{" "}
-          {window.LocationMacksInn.getLocationWeatherConditions()}
-          <br />
-          Cloud Rating: {window.LocationMacksInn.getLocationCloudRating()}%
-          <br />
-          Current Temp: {window.LocationMacksInn.getLocationTemp()} degrees
-          <br />
-          Current Wind: {window.LocationMacksInn.getLocationWind()} MpH
-        </p>
-      ),
-      Icon: customIconRed,
-    },
-    {
-      geocode: [44.620299, -111.373187],
-      popUp: (
-        <p>
-          Location: {window.LocationHenrysLakeBoatLaunch.getName()}
-          <br />
-          Current Weather Conditions:{" "}
-          {window.LocationHenrysLakeBoatLaunch.getLocationWeatherConditions()}
-          <br />
-          Cloud Rating:{" "}
-          {window.LocationHenrysLakeBoatLaunch.getLocationCloudRating()}%
-          <br />
-          Current Temp: {window.LocationHenrysLakeBoatLaunch.getLocationTemp()}{" "}
-          degrees
-          <br />
-          Current Wind: {window.LocationHenrysLakeBoatLaunch.getLocationWind()}{" "}
-          MpH
-        </p>
-      ),
-      Icon: customIconRed,
-    },
-    {
-      geocode: [44.83127, -111.514738],
-      popUp: (
-        <p>
-          Location: {window.LocationThreeDollarBridge.getName()}
-          <br />
-          Current Weather Conditions:{" "}
-          {window.LocationThreeDollarBridge.getLocationWeatherConditions()}
-          <br />
-          Cloud Rating:{" "}
-          {window.LocationThreeDollarBridge.getLocationCloudRating()}%
-          <br />
-          Current Temp: {window.LocationThreeDollarBridge.getLocationTemp()}{" "}
-          degrees
-          <br />
-          Current Wind: {window.LocationThreeDollarBridge.getLocationWind()} MpH
-        </p>
-      ),
-      Icon: customIconRed,
-    },
-  ];
-//
+  
   if (loading) {
     // Display a loading indicator while data is being fetched
     return <div>Fetching Live Data...</div>;
