9ced697eb52c69dd7423b9b9eca69efb7106fe5f
Max Watson <maxwats4@gmail.com>
2024-08-16 11:42:52 -0600

    Changed branding to FishEye and chnaged the instructions box to a colapsible box

--- src/layouts/map/components/InfoContainer.js
+++ src/layouts/map/components/InfoContainer.js
@@ -1,29 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css"; // Import your styles
 
 const InfoContainer = () => {
+  const [isOpen, setIsOpen] = useState(true); // State to control visibility
+
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen); // Toggle the visibility state
+  };
+
   return (
     <div className="info-container">
-      <h2>How the App Works!</h2>
-      <p>
-        Welcome to FishJournal! This app showcases various fishing locations
-        with real-time weather and river conditions.
-      </p>
-      <p>
-        Each marker on the map represents a fishing location, and you can click
-        on them to view details about the location.
-      </p>
-      <h3>Is Your Location Not Here?</h3>
-      <p>
-        Click the "Switch Mode" button to switch to the Click-n-find map which
-        allows you to click anywhere and get local river conditions. Click the "Save Location" button to save locations to be viewed on the previous map. 
-      </p>
-      <h2>Have Feedback?</h2>
-      <p>
-        Click the following link to submit an improvment request. 
-        </p>
-        <a href="https://forms.gle/NjNcb69YMH9tbMqbA">Feedback Form</a>
+      {isOpen && (
+        <div className="info-content">
+          <span className="close-icon" onClick={toggleCollapse}>
+            &times;
+          </span>
+          <h2>How the App Works!</h2>
+          <p>
+            Welcome to FishEye! This app showcases various fishing locations
+            with real-time weather and river conditions.
+          </p>
+          <p>
+            Each marker on the map represents a fishing location, and you can
+            click on them to view details about the location.
+          </p>
+          <h3>Is Your Location Not Here?</h3>
+          <p>
+            Click the "Switch Mode" button to switch to the Click-n-find map
+            which allows you to click anywhere and get local river conditions.
+            Click the "Save Location" button to save locations to be viewed on
+            the previous map.
+          </p>
+          <h2>Have Feedback?</h2>
+          <p>Click the following link to submit an improvement request.</p>
+          <a href="https://forms.gle/NjNcb69YMH9tbMqbA">Feedback Form</a>
+        </div>
+      )}
 
+      {!isOpen && (
+        <span className="expand-icon" onClick={toggleCollapse}>
+          &#43;
+        </span>
+      )}
     </div>
   );
 };
